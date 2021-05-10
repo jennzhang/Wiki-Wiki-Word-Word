e5218854c8244e451fe3439fa9e57844ec9e3238
Jennifer Zhang <zhangjen@flip2.engr.oregonstate.edu>
2021-05-10 09:48:15 -0700

    website files added, home page logic still outstanding

---
+++ public_html/microservice.js
@@ -0,0 +1,138 @@
+
+//add form handler
+document.addEventListener('DOMContentLoaded', bindSearchForm);
+
+// bindSearchForm()
+// handler for the Wikipedia Search Gui form submission
+// on submit, sends the search request to the Wiki Wiki 
+// Word Word api and then shows the results in a modal
+function bindSearchForm() 
+{
+	document.getElementById("search_form").onsubmit = e => {
+		e.preventDefault();
+		
+		// build api call
+		let search_term = document.getElementById("search_term").value; 
+		// format the search term. change any spaces to "%20"
+    // split() citation: https://www.w3schools.com/jsref/jsref_split.asp
+    // join() citation: https://www.w3schools.com/jsref/jsref_join.asp
+    search_term = search_term.split(" ").join("%20");
+		
+		
+		let container = document.getElementById("results_JSON");
+		getSearchResults(search_term).then( resultsJSON => 
+		{
+			// Get the results modal
+			var modal = document.getElementById("resultsModal");
+
+			var resultsList = document.getElementById("resultsList");
+			// clear out any old data
+			while ( resultsList.firstChild ) { resultsList.removeChild(resultsList.firstChild) };
+			
+			// add new data
+			// if there are no results, display message 
+			let results_string = "";
+			if (resultsJSON.numResults == 0) 
+			{ 
+				results_string = "There are no results.";
+				resultsList.textContent = results_string;
+			}
+			else
+			{	
+				// for each result, print the title page as a subheading, 
+				// linking to the Wikipedia page, then print the extract
+				for ( const page in resultsJSON.pages )
+				{				
+					let pageNode = document.createElement('p');
+				
+					// create linked page title
+					let pageTitle = document.createElement('a');
+					let title = document.createTextNode(resultsJSON.pages[page].title);
+					pageTitle.append(title);
+					pageTitle.href = resultsJSON.pages[page].url;
+				
+					// create page summary
+					let pageSummary = document.createElement('p');
+					let summary = document.createTextNode(resultsJSON.pages[page].extract);
+					pageSummary.append(summary);
+				
+				// add to the new pageNode
+					pageNode.appendChild(pageTitle);
+					pageNode.appendChild(pageSummary);
+				
+				//add to the modal
+				document.getElementById("resultsList").appendChild(pageNode);
+				}
+			}
+		
+			// Show the modal
+			modal.style.display = "block";
+
+			// Get the <span> element that closes the modal
+			var span = document.getElementById("closeResultsModalButton");
+
+			// When the user clicks on <span> (x), empty the form inputs, then close the modal
+			span.onclick = function() 
+			{
+					document.getElementById("search_term").value = "";
+					modal.style.display = "none";
+			}
+
+			// When the user clicks anywhere outside of the modal, empty the form, close the modal
+			window.onclick = function(event) 
+			{
+					if (event.target == modal) 
+					{
+							document.getElementById("search_term").value = "";
+							modal.style.display = "none";
+					}
+			}
+				})
+				.catch ( error => alert(error) );
+	}	
+}
+
+function getSearchResults(search_term)
+{
+	let url = "https://flip2.engr.oregonstate.edu:7764/api/?search_term=";
+	url =  url + search_term;
+
+	// use fetch to send the request and get the results (Wikipedia pages)
+	let response = fetch(url)
+		.then(response => response.json())
+		.catch(error => alert(error.message));
+	
+	return response;
+}
+
+// function displayResultsModal(resultsJSON){
+    // // Get the results modal
+    // var modal = document.getElementById("resultsModal");
+
+		// // create the results text block
+		// let results_string = "";
+		// if (resultsJSON.numResults == 0) { results_string = "There are no results."}
+		// else { results_string = JSON.stringify(resultsJSON);}
+		// modal.textContent = results_string;
+		
+    // // Show the modal
+		// modal.style.display = "block";
+
+		// // Get the <span> element that closes the modal
+		// var span = document.getElementById("closeResultsModalButton");
+
+		// // When the user clicks on <span> (x), empty the form inputs, then close the modal
+		// span.onclick = function() 
+		// {
+				// modal.style.display = "none";
+		// }
+
+		// // When the user clicks anywhere outside of the modal, empty the form, close the modal
+		// window.onclick = function(event) 
+		// {
+				// if (event.target == modal) 
+				// {
+						// modal.style.display = "none";
+				// }
+		// }
+// }
---
+++ public_html/test_server/scraper.js
@@ -0,0 +1,173 @@
+// CS361_Spring2021_400 - Jennifer Zhang
+// Wikipedia scraper module - get wiki links and blurbs related to a search term
+const fetch = require('node-fetch'); // fetch() is browser implemented, so using a third party library for its use with nodeJS
+
+module.exports.getWikiSearchResults = getWikiSearchResults;
+
+// functions ---------------------------------------------------------
+
+// getWikiSearchResults():
+// takes in a search term and returns a list of 
+// 0-10 related Wikipedia articles, with a rank 
+// index indicating the relevancy (1 is the highest
+// relevancy and 10 is the lowest). For each article,
+// a pageid, title, short extract no longer than 
+// three sentences, and a url are returned.
+function getWikiSearchResults(searchTerm)
+{
+	// Wikipedia has a recommendation for their url builder. Used here, citation: https://www.mediawiki.org/wiki/API:Pageprops
+
+	// base url
+	var wikiAPISearchUrl = "https://en.wikipedia.org/w/api.php";
+	
+	// format the search term. change any spaces to "%20"
+	// split() citation: https://www.w3schools.com/jsref/jsref_split.asp
+	// join() citation: https://www.w3schools.com/jsref/jsref_join.asp
+	let term = searchTerm.split(" ").join("%20"); 
+	
+	// Note for page retrieval limit: allow up to 15 pagesto cover any disambiguation pages. Remove disambiguation pages in formatPages() via removePagesWithoutExtracts();
+	var params = 
+	{
+		action: "query",
+		format: "json",
+		generator: "search",
+		prop: "extracts|pageprops",
+		gsrlimit: "15",					// generator search results page limit. 
+		exsentences: "3",				// extract max sentences
+		exintro: "1",					// boolean: if the parameter is specified, regardless of value, it is considered true
+		explaintext: "1",				// also a boolean parameter
+		ppprop: "disambiguation", 		// speficies if a page is a disambiguation page
+		gsrsearch: term					// search term					
+	};
+
+	// for unauthenticated CORS request, set the origin parameter t0 "*"
+	// citation: https://www.mediawiki.org/wiki/API:Cross-site_requests
+	wikiAPISearchUrl = wikiAPISearchUrl + "?origin=*";
+	Object.keys(params).forEach( key => {wikiAPISearchUrl += "&" + key + "=" + params[key];});
+
+	// note: using the fetch as recommended by the Wikipedia API instead of an XMLHttpRequest object for brevity. MDN citation, if needed: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+	let wikipediaResponse = fetch(wikiAPISearchUrl)
+		.then( wikiResponse => {return wikiResponse.json();})
+		.then( wikiResponseJSON => {
+			
+			let response = {};
+	
+			if (wikiResponseJSON.query) 	// there are search results
+			{
+				response.pages = formatPages(wikiResponseJSON.query.pages);
+				response.numResults = Object.keys(response.pages).length;	
+			}
+			else	// there are no search results
+			{
+				response.numResults = 0;
+				response.pages = {};
+			}
+			return response;
+		})
+		.catch( error => {console.log(error);});
+	
+	return wikipediaResponse;
+};
+
+// removePagesWithoutExtracts() :
+// is a helper function that removes any Wikipedia
+// search results that are disambiguation pages, or
+// other pages that do not have extracts, and are 
+// pages only that list or link to other pages as
+// these pages do not have any direct information 
+// nor do they have meaningful extracts. The pages
+// indicated in disambiguation pages should already 
+// be included in the reset of the Wikipedia search
+// results set
+function removePagesWithoutExtracts(pagesObj)
+{
+	Object.keys(pagesObj).forEach( page => 
+	{
+		if (pagesObj[page].pageprops ||
+			pagesObj[page].extract.length == 0) 
+			{
+				// delete disambiguation pages and pages with
+				// empty extracts from the response.
+				delete pagesObj[page];
+			}
+	});
+	
+	return pagesObj;
+}
+
+// reorderIndexes():
+// is a helper function which is called after 
+// removePagesWithoutExtracts(); it reorders
+// the indexes of the Wikipedia search results 
+// so that they are contiguous and limits the
+// max search results to 10, if there are more
+// than 10 present.
+function reorderIndexes(pagesObj)
+{
+	// use an array to sort the indexes
+	let newIdxPages = [];
+	
+	// add each page to the reorder array, newIdxPages	
+	Object.keys(pagesObj).forEach( page => 
+	{
+		newIdxPages.push(pagesObj[page]);
+	});
+
+	// sort the array. Using MDN's approach for sorting numbers in javascript,
+	// citation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+	newIdxPages.sort((a, b) => a.index - b.index);
+	
+	// only keep the first ten pages. Note: removePagesWithoutExtracts() should have been called first
+	newIdxPages = newIdxPages.slice(0,10)
+	
+	// empty pagesObj and then rebuild
+	pagesObj = {};
+	for (idx in newIdxPages) pagesObj[Number(idx) + 1] = newIdxPages[idx]	// citation for Number(): https://www.w3schools.com/jsref/jsref_number.asp
+	
+	return pagesObj;
+}
+
+
+// trimAttributes():
+// is a helper function that is called after
+// reorderIndexes(); it trims the Wikipedia 
+// results set so that only the desired
+// original attributes are kept (pageid, 
+// title, extract), and add's the Wikipedia 
+// page url as a new attribute for each
+// Wikipedia page.
+function trimAttributes(pagesObj)
+{
+	// remove keys: index, ns
+	// add url using the title (not using the pageid)	
+	Object.keys(pagesObj).forEach( page => 
+	{
+		delete pagesObj[page].index;
+		delete pagesObj[page].ns;
+		
+		// add the url to the response: format page titles so that spaces become underscores
+		// split() citation: https://www.w3schools.com/jsref/jsref_split.asp
+		// join() citation: https://www.w3schools.com/jsref/jsref_join.asp
+		pagesObj[page].url = "https://en.wikipedia.org/wiki/" + pagesObj[page].title.split(" ").join("_");
+	});
+	
+	// console.log(pagesObj);
+	
+	return pagesObj;
+}
+
+// formatPages():
+// formats the Wiki Wiki Word Word Response 
+// JSON in preparation to send to the caller 
+// of the Wiki Wiki Word Word API endpoint.
+// The function is only called if Wikipedia
+// returns search results for the search term.
+function formatPages(pagesObj)
+{
+	// format the Wiki Wiki Word Word Response JSON
+	let pages = trimAttributes(
+					reorderIndexes(
+							removePagesWithoutExtracts(pagesObj)));
+	
+	return pages;
+}
